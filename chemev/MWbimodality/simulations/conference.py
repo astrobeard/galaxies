189714868ffc6888fd0aa53feb032f46c3e27664
astrobeard <giganano9@gmail.com>
2020-07-31 15:01:49 -0400

    plots: new models added

--- chemev/MWbimodality/simulations/conference.py
+++ chemev/MWbimodality/simulations/conference.py
@@ -26,9 +26,19 @@ TSTAR_1 = 0.04 # tau_star normalization in linear phase
 TSTAR_2 = 2. # tau_star normalization in exponential phase 
 ZONE_WIDTH = 0.25 # width of each zone in kpc 
 RSCALE = 3 # scale radius of this disk model 
-DT = 0.01 # The timestep size in Gyr 
+DT = 0.05 # The timestep size in Gyr 
 TSTAR_NORM = 0.2 
 ALPHA = 0.1 
+
+
+FMOL = 0.35 # the ratio of molecular to total gas content in the disk 
+RS_MOL = 3. # scale radius of molecular disk in kpc 
+RS_HI = 6. # scale radius of the HI disk in kpc 
+R_SF = 15.5 # The radius of the star forming disk in kpc 
+R_CMZ = 0.5 # radius of the central molecular zone in kpc 
+R_HI = 20 # radius of the HI disk in kpc 
+TAU_STAR_MOL = 0.5 # depletion timescale of molecular gas 
+TSWITCH = 1. # linear to exponential switch in Gyr 
 
 
 def tau_in(rgal): 
@@ -70,50 +80,149 @@ class sfe:
 	""" 
 
 	def __init__(self, rgal): 
-		# self._norm = tau_star(rgal, norm = TSTAR_NORM) 
-		self._switch = TIME_SWITCH 
-		self._tstar1 = tau_star(rgal, norm = TSTAR_1) 
-		self._tstar2 = tau_star(rgal, norm = TSTAR_2) 
-		self._rgal = rgal 
+		if rgal <= R_CMZ: 
+			self._tstar = TAU_STAR_MOL 
+		elif rgal <= R_SF: 
+			self._tstar = TAU_STAR_MOL * (1 + sfe.amp_ratio() * 
+				m.exp(-(rgal - R_CMZ) / RS_HI) / 
+				m.exp(-rgal / RS_MOL) 
+			) 
+		else: 
+			self._tstar = 1.e6 
 
 	def __call__(self, time): 
-		# if time < self._switch: 
-		return self._tstar1 
-		# else: 
-		# 	return self._tstar1 + (self._tstar2 - self._tstar1) * (
-		# 		1 - m.exp(-(time - self._switch) / tau_in(self._rgal)) 
-		# 	) 
-		# if time < self._switch: 
-		# 	return self._norm  
-		# else: 
-		# 	return self._norm + (time - self._switch) / (12.8 - self._switch) 
+		return self._tstar 
+
+	@staticmethod 
+	def amp_ratio(): 
+		r""" 
+		The amplitude ratio of the HI to molecular gas disks 
+		""" 
+		return (1 / FMOL - 1) * (
+			RS_MOL**2 * (1 - m.exp(-R_SF / RS_MOL)) - RS_MOL * R_SF * m.exp(
+				-R_SF / RS_MOL) 
+		) / (
+			R_CMZ * RS_HI - R_HI * RS_HI * m.exp(-(R_HI - R_CMZ) / RS_HI) + 
+			RS_HI**2 * (1 - m.exp(-(R_HI - R_CMZ) / RS_HI)) 
+		) 
+
+
+# class star_formation_history: 
+
+# 	r""" 
+# 	The functional form of the star formation history under this model 
+# 	""" 
+# 	def __init__(self, rgal): 
+# 		self._rgal = rgal 
+# 		self._norm = (2 * m.pi * rgal * m.exp(-rgal / RSCALE) * ZONE_WIDTH / 
+# 			TIME_SWITCH * (
+# 				# TIME_SWITCH / 2. + TSTAR_1 / TSTAR_2 * tau_in(rgal) * (
+# 				TIME_SWITCH / 2. +  tau_in(rgal) * (
+# 					m.exp(-TIME_SWITCH / tau_in(rgal)) - 
+# 					m.exp(-TIME_BINS[-1] / tau_in(rgal)) 
+# 				) 
+# 			)**(-1) 
+# 		) 
+
+# 	def __call__(self, time): 
+# 		if time < TIME_SWITCH: 
+# 			# return self._norm 
+# 			return self._norm * TIME_SWITCH  
+# 		else: 
+# 			# return self._norm * TIME_SWITCH * TSTAR_1 / TSTAR_2 * m.exp(
+# 			return self._norm * TIME_SWITCH * m.exp(
+# 				-(time - TIME_SWITCH) / tau_in(self._rgal)) 
 
 
 class star_formation_history: 
 
-	r""" 
-	The functional form of the star formation history under this model 
-	""" 
 	def __init__(self, rgal): 
-		self._rgal = rgal 
-		self._norm = (2 * m.pi * rgal * m.exp(-rgal / RSCALE) * ZONE_WIDTH / 
-			TIME_SWITCH * (
-				# TIME_SWITCH / 2. + TSTAR_1 / TSTAR_2 * tau_in(rgal) * (
-				TIME_SWITCH / 2. +  tau_in(rgal) * (
-					m.exp(-TIME_SWITCH / tau_in(rgal)) - 
-					m.exp(-TIME_BINS[-1] / tau_in(rgal)) 
-				) 
-			)**(-1) 
-		) 
+		self._timescale = star_formation_history.tau_sfh(rgal) 
+		self._norm = self.norm(rgal) 
 
 	def __call__(self, time): 
-		if time < TIME_SWITCH: 
-			# return self._norm 
-			return self._norm * TIME_SWITCH  
+		# linear-exponential form 
+		# return self._norm * time * m.exp(-time / self._timescale) 
+
+		# linear-then-exponential form 
+		if time <= TSWITCH: 
+			return self._norm * time 
 		else: 
-			# return self._norm * TIME_SWITCH * TSTAR_1 / TSTAR_2 * m.exp(
-			return self._norm * TIME_SWITCH * m.exp(
-				-(time - TIME_SWITCH) / tau_in(self._rgal)) 
+			return self._norm * TSWITCH * m.exp(-(time - TSWITCH) / 
+				self._timescale) 
+
+	@property 
+	def timescale(self): 
+		return self._timescale 
+
+	@staticmethod 
+	def tau_sfh(rgal): 
+		r""" 
+		Star formation history timescale as a function of galactocentric 
+		radius. 
+
+		Parameters 
+		----------
+		rgal : real number  
+			Galactocentric radius in kpc 
+
+		Returns 
+		-------
+		tau_sfh : real number  
+			The e-folding timescale of the star formation history at that 
+			radius in Gyr. 
+		""" 
+		return 3 + (rgal + 1e-12) / 2.5 
+
+	def norm(self, rgal): 
+		r""" 
+		The normalization of the star formation history as a function of 
+		galactocentric radius. 
+
+		Parameters 
+		----------
+		rgal : real number 
+			Galactocentric radius in kpc 
+
+		Returns 
+		-------
+		norm : real number 
+			The normalization of the star formation history in 
+			:math:`M_\odot/yr`. 
+		""" 
+
+		# norm for linear-exponential evolution 
+		# return self.timescale**(-2) * (
+		# 	1 - (1 + TIME_BINS[-1] / self.timescale) * m.exp(
+		# 		-TIME_BINS[-1] / self.timescale
+		# 	) 
+		# )**(-1) * rgal * m.exp(-rgal / RS_MOL) 
+
+		# norm for linear-then-exponential evolution 
+		return rgal * m.exp(-rgal / RS_MOL) * ZONE_WIDTH * (
+			0.5 * TSWITCH**2 + 
+			TSWITCH * star_formation_history.tau_sfh(rgal) * (
+				1 - m.exp(-(TIME_BINS[-1] - TSWITCH) / 
+					star_formation_history.tau_sfh(rgal)
+				) 
+			) 
+		)**(-1) 
+
+
+class constant_sfh: 
+
+	r""" 
+	Constant star formation history embedded within the molecular plus 
+	neutral hydrogen star-forming disk. 
+	""" 
+
+	def __init__(self, rgal): 
+		self._mdotstar0 = 1. 
+		self._rgal = rgal 
+
+	def __call__(self, time): 
+		return self._mdotstar0 * 2 * m.pi * self._rgal * m.exp(-self._rgal / 
+			RS_MOL) * ZONE_WIDTH 
 
 
 class infall_history: 
@@ -142,6 +251,66 @@ class infall_history:
 		return self._norm * m.exp(-time / self._tau_in) 
 
 
+
+class diskmigration(hydrodisk.hydrodiskstars): 
+
+	r""" 
+	Subclassed hydrodiskstars object to write extra analog star particle data 
+	to an output file. 
+	""" 
+
+	def __init__(self, radbins, mode = "linear", filename = "stars.out"): 
+		super().__init__(radbins, mode = mode) 
+		if isinstance(filename, str): 
+			self._file = open(filename, 'w') 
+			self._file.write("# zone_origin\ttime_origin\tzfinal\n") 
+		else: 
+			raise TypeError("Filename must be a string. Got: %s" % (
+				type(filename))) 
+
+		# Multizone object automatically swaps this to True in setting up 
+		# its stellar population zone histories 
+		self.write = False 
+
+	def __call__(self, zone, tform, time): 
+		if tform == time: 
+			super().__call__(zone, tform, time) # reset analog star particle 
+			if self.write: 
+				if self.analog_index == -1: 
+					finalz = 100 
+				else: 
+					finalz = self.analog_data["zfinal"][self.analog_index] 
+				self._file.write("%d\t%.2f\t%.2f\n" % (zone, tform, finalz)) 
+			else: pass 
+			return zone 
+		else: 
+			return super().__call__(zone, tform, time) 
+
+	def close_file(self): 
+		r""" 
+		Closes the output file - should be called after the multizone model 
+		simulation runs. 
+		""" 
+		self._file.close() 
+
+	@property 
+	def write(self): 
+		r""" 
+		Type : bool 
+
+		Whether or not to write out to the extra star particle data output 
+		file. For internal use by the vice.multizone object only. 
+		""" 
+		return self._write 
+
+	@write.setter 
+	def write(self, value): 
+		if isinstance(value, bool): 
+			self._write = value 
+		else: 
+			raise TypeError("Must be a boolean. Got: %s" % (type(value))) 
+
+
 class diskmodel(vice.multizone): 
 
 	def __init__(self): 
@@ -154,20 +323,23 @@ class diskmodel(vice.multizone):
 		# self.migration.stars = tracers.UWhydro(TIME_BINS, RAD_BINS, 
 		# 	n_stars = self.n_stars, 
 		# 	filename = "%s_extra_tracer_data.out" % (self.name)) 
-		self.migration.stars = hydrodisk.linear(RAD_BINS) 
+		# self.migration.stars = hydrodisk.hydrodiskstars(RAD_BINS) 
+		self.migration.stars = diskmigration(RAD_BINS, 
+			filename = "%s_extra_tracer_data.out" % (sys.argv[1])) 
 		for i in range(self.n_zones): 
 			# self.zones[i].func = infall_history(ZONE_WIDTH * (i + 0.5)) 
 			# self.zones[i].mode = "ifr" 
 			self.zones[i].func = star_formation_history(ZONE_WIDTH * (i + 0.5)) 
+			# self.zones[i].func = constant_sfh(ZONE_WIDTH * (i + 0.5)) 
 			self.zones[i].mode = "sfr" 
 			self.bins = np.linspace(-3, 1, 401) 
 			self.zones[i].elements = ["fe", "o"] 
 			self.zones[i].dt = DT 
 			self.zones[i].Mg0 = 0 
-			self.zones[i].schmidt = True 
+			# self.zones[i].schmidt = True 
 			if i > 61: 
 				self.zones[i].func = lambda t: 0 
-				self.zones[i].tau_star = 100 
+				self.zones[i].tau_star = 1.e6 
 				self.zones[i].eta = 100 
 				for j in self.zones[i].elements: 
 					self.zones[i].entrainment.agb[j] = 0 
@@ -179,7 +351,7 @@ class diskmodel(vice.multizone):
 
 	def run(self): 
 		super().run(np.linspace(0, 12.8, 257), overwrite = True) 
-		# self.migration.stars.close_file() 
+		self.migration.stars.close_file() 
 		# pass 
 
 
@@ -235,8 +407,9 @@ def eta(rgal, corrective = 0):
 
 if __name__ == "__main__": 
 	for i in RAD_BINS[:60]: 
-		print("R = %.2f kpc ; tau_in = %.2f" % (
-			i + ZONE_WIDTH / 2, tau_in(i + ZONE_WIDTH / 2))) 
+		print("R = %.2f kpc ; tau_sfh = %.2f" % (
+			i + ZONE_WIDTH / 2, 
+			star_formation_history.tau_sfh(i + ZONE_WIDTH / 2))) 
 	diskmodel().run() 
 
 
